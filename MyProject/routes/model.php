0c5b9d80fdd5fe635a2b2f66c9e457f8e9133971
tiendung78 <56835262+tiendung78@users.noreply.github.com>
2021-10-05 11:39:36 +0700

    Delete MyProject directory

--- MyProject/routes/model.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::group(['prefix'=>'/model/author'], function(){
-    Route::get('/index', [\App\Http\Controllers\Model\AuthorController::class, 'index']);
-    Route::get('/view_add', [\App\Http\Controllers\Model\AuthorController::class, 'viewAdd']);
-
-});
